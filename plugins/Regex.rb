d87e1419066d1785e692e95eb6991d2a1de8e9ca
Jake Vandereay <gvandereay@gmail.com>
2018-08-01 01:31:08 -0700

    add nick tags to regex replies

--- plugins/Regex.rb
+++ plugins/Regex.rb
@@ -13,27 +13,27 @@ class Regex
   def execute(m)
     @log[m.channel] ||= [] #init if none
     regex_str = m.message[/\/(.*)\//, 1]
-    replace_str = m.message[/>>(.*)/, 1]
+    replace_str = m.message[/\/.*\/(.*)/, 1]
     if regex_str and m.message[/^!.*/]
       @log[m.channel].each do |msg|
-        result = msg[/#{regex_str}/]
+        result = msg[:msg][/#{regex_str}/]
         if result
-          unless replace_str
-            m.reply result
+          unless replace_str && !replace_str.empty?
+            m.reply "(#{msg[:nick]}) #{msg[:msg]}"
           else
-            m.reply msg.gsub /#{regex_str}/, replace_str
+            m.reply "(#{msg[:nick]}) #{msg[:msg].gsub /#{regex_str}/, replace_str}"
           end
           return
         end
       end
       m.reply "No matches for /#{regex_str}/"
     else
-      @log[m.channel] << m.message #log it
-      @log[m.channel] = @log[m.channel][0,25]#limit it
+      @log[m.channel] << {msg: m.message, nick: m.user} #log it
+      @log[m.channel].shift if @log[m.channel].size>24
     end
   end
-  match /return_log/, method: :return_log
+  match /regex_channel_buffer/, method: :return_log
   def return_log(m)
-    m.reply @log[m.channel].join(' /\/ ')
+    m.reply @log[m.channel].map{|l| "(#{l[:nick]}) #{l[:msg]}"}.join(', ')
   end
 end
